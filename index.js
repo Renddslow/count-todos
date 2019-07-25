e62a9d9579880807807eda40be5b04be3d747652
Adam Schlichtmann <aschlichtmann@dmsi.com>
2019-07-25 18:01:59 -0500

    Added Functionality to calculate average age of Todo

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { execSync } = require('child_process');
+const exec = require('child_process').exec;
 const chalk = require('chalk');
 
 module.exports = (filePath, options) => {
@@ -37,8 +38,36 @@ module.exports = (filePath, options) => {
 	if (optionsObj.verbose) {
 		console.log(chalk.gray(cmd));
 	}
-
+	var diff = [];
 	const resultBuffer = execSync(cmd);
+	const other_cmd = "grep -inRw -E 'TODO' . --exclude-dir={node_modules,test}"
+	const foo = function(cb){
+		exec(other_cmd, (err, stdout, stderr) => {
+			if (err) {
+				console.error(`exec error: ${err}`);
+				return cb(err);
+			}
+			cb(null, {stdout,stderr});
+		});
+	}
+
+	foo(function(err, {stdout,stderr}) {
+		if (err) {
+			console.log("ERROR in foo");
+			return;
+		} 
+		const dates = stdout.match(/[0-9]{2}\/[0-9]{2}\/[0-9]{2}/g);
+		dates.forEach((date) => {
+			var oneDay = 24*60*60*1000;
+			var newDate = new Date(date);
+			const today = new Date();
+			diff.push(Math.round(Math.abs((newDate.getTime() - today.getTime())/(oneDay))));
+		});
+		const reducer = (accumulator, currentValue) => accumulator + currentValue;
+		const average = diff.reduce(reducer) / diff.length;
+		console.log(`Average age of TODO: ${average}`);
+ });
+
 	const resultRaw = resultBuffer && resultBuffer.toString();
 
 	if (!resultRaw) {
